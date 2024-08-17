64aab9e0f221a8e8941e49c47d164353aa3eba1c
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-17 15:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-document.getElementById('getWeatherBtn').addEventListener('click', getWeatherData);
+document.getElementById('getWeatherBtn').addEventListener('click', getCurrentWeather);
 
-function getWeatherData() {
-    const apiKey = '8cc3a796064ba5e1cc226c5974eae78a'; // Replace with your OpenWeatherMap API key
+function getCurrentWeather() {
+    const apiKey = 'e467712b257e418838be97cc881a71de'; // Replace with your OpenWeatherMap API key
     const city = 'London';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
